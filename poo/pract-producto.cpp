7ddda6eb1d8d4d920e2e0fa5f618a993404e72ef
Patricio Quishpe <109158582+PatricioInsider@users.noreply.github.com>
2023-01-06 09:45:28 -0500

    Update static

--- poo/pract-producto.cpp
+++ poo/pract-producto.cpp
@@ -6,60 +6,61 @@ using namespace std;
 class Producto
 {
 private:
-    string nombre,fecha,lote;
+    string nombre, fecha, lote;
+
 public:
     Producto(/* args */);
     ~Producto();
     void leer()
     {
-        cout<<"Nombre del producto:"<<endl;
-        cin>>nombre;
-        cout<<"Fecha de caducidad formato (AAAAMMDD): "<<endl;
-        cin>>fecha;
-        cout<<"Ingresa el numero de lote: "<<endl;
-        cin>>lote;
-        
+        cout << "Nombre del producto:" << endl;
+        cin >> nombre;
+        cout << "Fecha de caducidad formato (AAAAMMDD): " << endl;
+        cin >> fecha;
+        cout << "Ingresa el numero de lote: " << endl;
+        cin >> lote;
     }
 
     void mostrar()
     {
-        cout<<nombre<<"\t"<<fecha<<"\t"<<lote<<"\t";
+        cout << nombre << "\t" << fecha << "\t" << lote << "\t";
     }
-    
 };
 
 Producto::Producto(/* args */)
 {
+    nombre = fecha = lote = "";
 }
 
 Producto::~Producto()
 {
 }
 
-class ProductoFresco: public Producto
+class ProductoFresco : public Producto
 {
 private:
-	string id;
+    string id;
+
 public:
     ProductoFresco(/* args */);
     ~ProductoFresco();
     void leer()
     {
-        leer();
-        cout<<"Identificador: "<<endl;
-        cin>>id;
-        cout<<"***"<<endl;
+        Producto::leer();
+        cout << "Identificador: " << endl;
+        cin >> id;
+        cout << "***" << endl;
     }
 
     int mostrar()
     {
-        mostrar();
-        cout<<id<<"\t"<<endl;
+        Producto::mostrar();
+        cout << id << "\t" << endl;
     }
     string getid()
     {
-    	return id;
-	}
+        return id;
+    }
 };
 
 ProductoFresco::ProductoFresco(/* args */)
@@ -70,26 +71,26 @@ ProductoFresco::~ProductoFresco()
 {
 }
 
-class ProductoCongelado: public Producto
+class ProductoCongelado : public Producto
 {
 private:
     float temperatura;
+
 public:
     ProductoCongelado(/* args */);
     ~ProductoCongelado();
     void leer()
     {
-        leer();
-        cout<<"Temperatura de congelacion recomendada(grados Ccelsius): "<<endl;
-        cin>>temperatura;
-        cout<<"***"<<endl;
+        Producto::leer();
+        cout << "Temperatura de congelacion recomendada(grados Ccelsius): " << endl;
+        cin >> temperatura;
+        cout << "***" << endl;
     }
 
     void mostrar()
     {
-        mostrar();
-        cout<<temperatura<<"\t"<<endl;
-        
+        Producto::mostrar();
+        cout << temperatura << "\t" << endl;
     }
     float gettemperatura()
     {
@@ -108,118 +109,117 @@ ProductoCongelado::~ProductoCongelado()
 class Empresa
 {
 private:
-    string nombre,direccion; 
-    ProductoCongelado oCongelado[100];
-    ProductoFresco oFresco[100];
-    int nCongelado,nFresco;
+    string nombre, direccion;
+    ProductoCongelado *oCongelado[100];
+    ProductoFresco *oFresco[100];
+    int nCongelado, nFresco;
+
 public:
     Empresa(/* args */);
     ~Empresa();
-    
+
     void ing_congelado()
     {
-    	for (int i = 0; i < nCongelado; i++)
+        for (int i = 0; i < nCongelado; i++)
         {
-        	cout<<"Ingresa Datos Del Producto Congelado "<<i+1<<endl;
-            oCongelado[i].leer();
+            cout << "Ingresa Datos Del Producto Congelado " << i + 1 << endl;
+            oCongelado[i] = new ProductoCongelado;
+            oCongelado[i]->leer();
         }
-	}
-	void ing_fresco()
-	{
-		for (int i = 0; i < nFresco; i++)
+    }
+    void ing_fresco()
+    {
+        for (int i = 0; i < nFresco; i++)
         {
-        	cout<<"Ingresa Datos Del Producto Frescos "<<i+1<<endl;
-            oFresco[i].leer();
+            cout << "Ingresa Datos Del Producto Frescos " << i + 1 << endl;
+            oFresco[i] = new ProductoFresco;
+            oFresco[i]->leer();
         }
-	}
+    }
     void leer()
     {
-    	cout<<"____________________________________________"<<endl;
-        cout<<"Empresa Agroalimentaria"<<endl;
-		cout<<"Ingreso de Datos totales"<<endl;
-        cout<<"Nombre de la empresa: "<<endl;
-        cin>>nombre;
-        cout<<"Direccion:"<<endl;
-        cin>>direccion;
-        cout<<"Cantidad de productos congelados"<<endl;
-        cin>>nCongelado;
-        while (nCongelado<0)
+        cout << "____________________________________________" << endl;
+        cout << "Empresa Agroalimentaria" << endl;
+        cout << "Ingreso de Datos totales" << endl;
+        cout << "Nombre de la empresa: " << endl;
+        cin >> nombre;
+        cout << "Direccion:" << endl;
+        cin >> direccion;
+        cout << "Cantidad de productos congelados" << endl;
+        cin >> nCongelado;
+        while (nCongelado < 0)
         {
-        	cout<<"Ingresa un valor positivo"<<endl;
-        	cin>>nCongelado;
-		}
+            cout << "Ingresa un valor positivo" << endl;
+            cin >> nCongelado;
+        }
         ing_congelado();
         system("pause");
         system("cls");
-        cout<<"Cantidad de productos frescos"<<endl;
-        cin>>nFresco;
-        while (nFresco<0)
+        cout << "Cantidad de productos frescos" << endl;
+        cin >> nFresco;
+        while (nFresco < 0)
         {
-        	cout<<"Ingresa un valor positivo"<<endl;
-        	cin>>nFresco;
-		}
+            cout << "Ingresa un valor positivo" << endl;
+            cin >> nFresco;
+        }
         ing_fresco();
         system("pause");
         system("cls");
-        
     }
     int ubi(string bus)
-	{
-    	int pos= -1;
-        int i=0;
-        if (nFresco>0)
-        cout<<"Buscando..."<<endl;
+    {
+        int pos = -1;
+        int i = 0;
+        if (nFresco > 0)
+            cout << "Buscando..." << endl;
         {
-        	while ((pos==-1)&&(i<nFresco))
-        	{
-            if (oFresco[i].getid()==bus)
+            while ((pos == -1) && (i < nFresco))
             {
-                pos=i;
+                if (oFresco[i]->getid() == bus)
+                {
+                    pos = i;
+                }
+                i++;
             }
-            i++;
-        	}
-        	return pos;
-				
-		}
-	}
+            return pos;
+        }
+    }
     void bus_fresco()
     {
-    	cout<<"____________________________________________"<<endl;
-    	cout<<"Buscar un producto Fresco por Identificador"<<endl;
+        cout << "____________________________________________" << endl;
+        cout << "Buscar un producto Fresco por Identificador" << endl;
         string bus;
-        cout<<"id del Producto Fresco a Buscar"<<endl;
-        cin>>bus;
-        //encabezado y verificar si hay o no hay elementos en la lista
-        cout<<"Nombre\t"<<"F Cadu\t"<<"Iden \t"<<endl;
-		int aux=ubi(bus);
-		if (aux==-1)
-		{
-			cout<<"No se encontro el producto"<<endl;
-		}else{
-			oFresco[aux].mostrar();
-		}
-		
-        
-
+        cout << "id del Producto Fresco a Buscar" << endl;
+        cin >> bus;
+        // encabezado y verificar si hay o no hay elementos en la lista
+        cout << "Nombre\t"
+             << "F Cadu\t"
+             << "Iden \t" << endl;
+        int aux = ubi(bus);
+        if (aux == -1)
+        {
+            cout << "No se encontro el producto" << endl;
+        }
+        else
+        {
+            oFresco[aux]->mostrar();
+        }
     }
-    //Temperaturas <=-4
+    // Temperaturas <=-4
 
     void tem_congelado()
     {
-        cout<<"____________________________________________"<<endl;
-		cout<<"Todos los productos menores o iguales a -4 de temperatura"<<endl;
-        //Impresion de productos congelados -4 
-        cout<<"Nombre\t F Cad\t Lote\t Temp\t"<<endl;
-        for (int i = 0; i <nCongelado ; i++)
+        cout << "____________________________________________" << endl;
+        cout << "Todos los productos menores o iguales a -4 de temperatura" << endl;
+        // Impresion de productos congelados -4
+        cout << "Nombre\t F Cad\t Lote\t Temp\t" << endl;
+        for (int i = 0; i < nCongelado; i++)
         {
-            if (oCongelado[i].gettemperatura()<=-4)
+            if (oCongelado[i]->gettemperatura() <= -4)
             {
-                oCongelado[i].mostrar();
+                oCongelado[i]->mostrar();
             }
-            
         }
-        
-
     }
 };
 
@@ -229,14 +229,25 @@ Empresa::Empresa()
 
 Empresa::~Empresa()
 {
+    for (int i = 0; i < nCongelado; i++)
+    {
+        delete oCongelado[i];
+    }
+
+    for (int i = 0; i < nFresco; i++)
+    {
+        delete oFresco[i];
+    }
 }
 
-
-
-int main ()
+int main()
 {
-    Empresa SanJuan; 
-    SanJuan.leer();
-    SanJuan.bus_fresco();
-    SanJuan.tem_congelado();
+    Empresa *ptr;
+    ptr = new Empresa;
+    ptr->leer();
+    ptr->bus_fresco();
+    ptr->tem_congelado();
+
+    delete ptr;
+    return 0;
 }
